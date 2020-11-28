4992b6e83254f5efb80db7d26f69e87ea8ccbda3
meshal <mmas3l222@gmail.com>
2020-11-28 22:16:31 +0300

    n

--- main_figure_Run_code.py
+++ main_figure_Run_code.py
@@ -207,26 +207,3 @@ def figures_EDA():#The figures of The figures of exploratory data analysis
 
 
 
-
-try:#Run the Program
-
-        print("1.The exploratory data analysis")
-        print("2.The figures of exploratory data analysis")
-        print("*******-Enter 0 to exit-*******")
-        user_option=int(input("Emter your option number: "))
-        if user_option==1:
-            EDA()
-
-        elif user_option==2:
-            figures_EDA()
-            plt.show()
-        else:
-            print("please enter the correct number!!!")
-
-
-except:
-        print("There is a problem")
-
-
-
-
